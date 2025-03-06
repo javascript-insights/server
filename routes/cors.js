7138e1b88dd679d0758d92415fbc093f67d6da91
Idan Mor <sidanmor@gmail.com>
2025-03-06 11:57:53 +0200

    add Preflight Requests

--- routes/cors.js
+++ routes/cors.js
@@ -47,4 +47,18 @@ router.get('/max-age', function (req, res) {
   res.send('This endpoint sets max age for preflight');
 });
 
+// CORS Preflight Requests
+router.options('/preflight', function (req, res) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Max-Age', '86400');
+  res.sendStatus(200);
+});
+
+router.post('/preflight', function (req, res) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.send('This endpoint handles preflight requests');
+});
+
 module.exports = router;
